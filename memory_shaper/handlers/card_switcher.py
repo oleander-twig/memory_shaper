f6df250651d22d04b613b97121937c8ec5e3c514
kaledinandrew <kaledin.andr@gmail.com>
2021-10-10 20:36:15 +0300

    Added text to templates

--- memory_shaper/handlers/card_switcher.py
+++ memory_shaper/handlers/card_switcher.py
@@ -5,4 +5,8 @@ from app import app
 
 @app.route('/')
 def card_front():
-    return render_template('card_front.html')
+    return render_template('card_front.html', text='some text')
+
+@app.route('/back')
+def card_back():
+    return render_template('card_back.html', text='some text')
